524f15cc06b6003cd40e159c463e99d309b3ad95
Paige Pittman <paigepittman@Paiges-MacBook-Air.local>
2017-06-16 15:11:54 -0700

    modal working needs styling

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 $("#searchbutton").on('click', function() {
     event.preventDefault();
 
-    $(".search-div").animate({ top: "-220px", left: "100px" }, 500);
+    $(".search-div").animate({ top: "-210px", left: "100px" }, 500);
     $(".logo").animate({ top: "-40px", left: "2px", "font-size": "30px" }, 500);
     // $(".logo").animate({ "font-size": "24px" }, 500);
     $(".searchbar").animate({width: "-=250px"}, 500);
